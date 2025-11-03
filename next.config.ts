e6a57de6b0a2726c0174426617f4883b54ea2f32
oOZeqOo <zeqoware@gmail.com>
2025-11-03 12:47:24 -0600

    Updated NextJS config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: true,
-  assetPrefix: '/regex-example/',
-  basePath: '/regex-example',
 };
 
 export default nextConfig;
